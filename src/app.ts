df6463de0fe2385a6f3dcbaee2e78eb9566f3c26
David <zdavid1@myges.fr>
2023-07-05 14:30:05 +0200

    start server

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,27 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+import {config} from "dotenv";
+import express from "express";
+import http from "http";
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+import cors from "cors";
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-});
+config();
+
+async function startServer(): Promise<void> {
+
+    const db = require('./utils/mysql.connector');
+    db.authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        }).catch((err: any) => {
+        console.error('Unable to connect to the database:', err);
+    });
+    const port = process.env.PORT || 3000;
+    const app = express();
+    const httpServer = http.createServer(app);
+    app.use(cors({origin: process.env.FRONT_URL}));
+
+
+    httpServer.listen(port, () => console.log(`Listening on port ${port}`));
+}
+
+startServer().catch(console.error);
---
+++ src/utils/mysql.connector.ts
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const Sequelize = require('sequelize');
+module.exports = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    port: 3306,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+
